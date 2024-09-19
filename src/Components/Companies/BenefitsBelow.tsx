0c24c9b0b345f6b5912d3efbb63cb4cd59ab9b4e
Mukesh Prajapati <145520335+mukes-kumar@users.noreply.github.com>
2024-09-19 09:31:34 +0530

    Flexible

--- src/Components/Companies/BenefitsBelow.tsx
+++ src/Components/Companies/BenefitsBelow.tsx
@@ -32,7 +32,7 @@ function BenefitsBelow() {
 
   return (
     <>
-      <ul className='opacity-[0.7] mb-[24px] flex flex-col pl-0 border-[0.5px] lg:w-[1070px] sm:w-[470px] md:w-[650px] w-[430px]  rounded-xl'>
+      <ul className='opacity-[0.7] mb-[24px] flex flex-col pl-0 border-[0.5px] lg:w-[920px] sm:w-[470px] md:w-[600px] w-[430px] rounded-xl'>
         {
           benifitsItem.map((item) => (
             <li key={item.id} className=' flex items-center p-5 bg-[#ffff] border-[1px] border-solid border-[#e9ecef] relative'>
